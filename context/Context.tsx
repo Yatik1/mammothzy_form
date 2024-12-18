90ec4796d80fb2230ddbd9621b8c248994d2780d
Yatik1 <yatiksrivastava1@gmail.com>
2024-12-18 17:09:04 +0530

    context

---
+++ context/Context.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { createContext, Dispatch, SetStateAction, useState } from "react"
+
+type ContextProps = {
+    selectedTag:string ,
+    setSelectedTag : Dispatch<SetStateAction<string>>
+}
+
+export const TagContext = createContext<ContextProps | null>(null)
+
+export default function ContextProvider({children}:{children: React.ReactNode}) {
+    const [selectedTag, setSelectedTag] = useState<string>("Activity Details")
+
+    return (
+        <TagContext.Provider 
+            value={{
+                selectedTag,
+                setSelectedTag
+            }}
+        >
+            {children}
+        </TagContext.Provider>
+    )
+}
