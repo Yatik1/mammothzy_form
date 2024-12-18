9dd18ea69cab4597400ad8cc582c1250432caab4
Yatik1 <yatiksrivastava1@gmail.com>
2024-12-18 17:08:07 +0530

    main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,83 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import ActivityForm from '@/components/forms/ActivityForm'
+import LocationForm from '@/components/forms/LocationForm'
+import { Separator } from '@/components/ui/separator'
+import useTagContext from '@/hooks/useTagContext'
+import { Flag, MapPin } from 'lucide-react'
+import React from 'react'
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+interface MenuProps {
+  icon:React.ReactNode
+  tag:string
+  form:React.ReactNode
 }
+
+const options:MenuProps[] = [
+  {
+    icon:<Flag />,
+    tag:"Activity Details",
+    form: <ActivityForm />
+  },
+  {
+    icon:<MapPin />,
+    tag:"Location Details",
+    form: <LocationForm />
+  }
+]
+
+function page() {
+
+  return (
+    <div className='w-full h-screen flex flex-col p-10'>
+      <h2 className='font-bold text-2xl tracking-tight'>Create new Activity</h2>
+      <div className="flex flex-grow w-full h-full">
+        <MenuOptions />
+        <Separator orientation={'vertical'} />
+        <FormSection />
+      </div>
+    </div>
+  )
+}
+
+function MenuOptions() {
+
+  const {setSelectedTag} = useTagContext()
+
+  return(
+      
+      <div className="w-1/5 h-full">
+          <div className="flex items-start flex-col p-2">
+          {options.map((option, index) => {
+            const {icon, tag} = option
+            return(
+              (
+                <div 
+                  className='w-[11.5rem] flex items-center justify-center text-gray-600 cursor-default pl-3 pr-6 py-1 hover:bg-gray-400/20 rounded-lg ' 
+                  key={index}
+                  onClick={() => setSelectedTag(tag)}
+                >
+                  <div className="p-2 flex items-center justify-center w-[2.2rem] h-[2.2rem]">{icon}</div>
+                  <p className='text-md tracking-tight'>{tag}</p>
+                </div>
+              )
+            )
+          })}
+          </div>
+    </div>
+
+  )
+}
+
+
+function FormSection() {
+  const {selectedTag} = useTagContext()
+
+  return (
+    <div className="w-full h-full">
+      {selectedTag === "Activity Details" ? <ActivityForm /> : <LocationForm />}
+    </div>
+  )
+}
+
+export default page
