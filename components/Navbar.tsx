d0891eb54c4a926d7bd2698216e3ea4fa8846973
Yatik1 <yatiksrivastava1@gmail.com>
2024-12-18 17:08:22 +0530

    navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import React from 'react'
+import logo from "../public/logo.png"
+import { User } from 'lucide-react'
+
+function Navbar() {
+  return (
+    <div className="w-full px-[5rem] py-4 flex justify-between items-center border-gray-200 border-b ">
+        <div className="flex items-center justify-center">
+            <Image
+                src={logo}
+                alt='Logo'
+                className='w-[12rem]'
+            />
+        </div>
+
+        <div className="flex justify-center items-center gap-2 cursor-pointer">
+            <div className="flex items-center justify-center w-7 h-7 rounded-full bg-gray-300 p-1 border border-black">
+                <User />
+            </div>
+            <p>Profile</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
