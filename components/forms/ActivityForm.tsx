b6c7749df4a389f59511fa6e6a005f2fc60e4faa
Yatik1 <yatiksrivastava1@gmail.com>
2024-12-19 17:25:01 +0530

    Activity form

--- components/forms/ActivityForm.tsx
+++ components/forms/ActivityForm.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import useTagContext from '@/hooks/useTagContext'
 
 function ActivityForm() {
+
+  const {setSelectedTag} = useTagContext()
+
   return (
-    <div>ActivityForm</div>
+    <div className='flex flex-col flex-grow gap-3 py-4 px-7 mx-7'>
+      
+      <h2 className="font-semibold text-lg">Activity Details</h2>
+      
+      
+      {/* <Button
+        onClick={() => setSelectedTag("Location Details")}
+      >Save and continue</Button> */}
+    </div>
+
   )
 }
 
