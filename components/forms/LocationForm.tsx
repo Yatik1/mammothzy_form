f9fca9ec95a52402bf9d70ecd42802f67a7bf33e
Yatik1 <yatiksrivastava1@gmail.com>
2024-12-19 17:25:08 +0530

    same with locations

--- components/forms/LocationForm.tsx
+++ components/forms/LocationForm.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import useTagContext from '@/hooks/useTagContext'
 
 function LocationForm() {
+  const {setSelectedTag} = useTagContext()
+  
   return (
-    <div>LocationForm</div>
+    <div>LocationForm
+
+<Button  onClick={() => setSelectedTag("Activity Details")}>Previous</Button>
+<Button>Submit</Button>
+    </div>
   )
 }
     
