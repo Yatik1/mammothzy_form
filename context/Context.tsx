19af3f67fdcf655d03443a15d1142b6bdf193adc
Yatik1 <yatiksrivastava1@gmail.com>
2024-12-21 17:04:59 +0530

    :: context cleared

--- context/Context.tsx
+++ context/Context.tsx
@@ -5,23 +5,20 @@ import { createContext, Dispatch, SetStateAction, useState } from "react"
 type ContextProps = {
     selectedTag:string ,
     setSelectedTag: Dispatch<SetStateAction<string>>,
-    isSubmitted : boolean,
-    setIsSubmitted: Dispatch<SetStateAction<boolean>>
+
 }
 
 export const TagContext = createContext<ContextProps | null>(null)
 
 export default function ContextProvider({children}:{children: React.ReactNode}) {
     const [selectedTag, setSelectedTag] = useState<string>("Activity Details")
-    const [isSubmitted, setIsSubmitted] = useState<boolean>(false)
+ 
 
     return (
         <TagContext.Provider 
             value={{
                 selectedTag,
                 setSelectedTag,
-                isSubmitted,
-                setIsSubmitted
             }}
         >
             {children}
