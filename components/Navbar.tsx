3a688557313415b2bb328b5ae235cd7621e12644
Yatik1 <yatiksrivastava1@gmail.com>
2024-12-21 17:02:48 +0530

    Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import Image from 'next/image'
 import React from 'react'
 import logo from "../public/logo.png"
 import { User } from 'lucide-react'
+import { useRouter } from 'next/navigation';
 
 function Navbar() {
+
+    const router = useRouter()
+
   return (
     <div className="w-full px-[5rem] py-4 flex justify-between items-center border-gray-200 border-b ">
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center cursor-pointer" onClick={() => router.push("/")}>
             <Image
                 src={logo}
                 alt='Logo'
